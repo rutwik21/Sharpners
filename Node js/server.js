3a8c3967df546ca9a0176668f09428b14241c94d
rutwik21 <rutwikkashid000@gmail.com>
2023-05-25 16:14:26 +0530

    added some features

--- Node js/server.js
+++ Node js/server.js
@@ -1,7 +1,22 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log("rutwik");
+
+    const url=req.url;
+
+    if(url === "/home"){
+        res.write('<h1>Welcome home</h1>')
+    }
+    else if(url === "/about"){
+        res.write('<h1>Welcome to about us page</h1>')
+    }
+    else if(url === "/node"){
+        res.write('<h1>Welcome to my node js project</h1>')
+    }
+
+    res.end();
+
+    
 });
 
 server.listen(4000);
