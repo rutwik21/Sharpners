532895bcf370d030aed49ed260cb630b1ec1f93e
rutwik21 <rutwikkashid000@gmail.com>
2023-06-09 18:40:07 +0530

    Delete functionality added!

--- e-com/controllers/admin.js
+++ e-com/controllers/admin.js
@@ -12,10 +12,14 @@ exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const discription = req.body.description;
+  const product = new Product(null, title, imageUrl, discription, price);
+  product
+    .save()
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -55,17 +59,22 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll()
+  .then(([products])=>{
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
-  });
+  })
+  .catch(err => console.log(err));
+  
+
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.deleteById(prodId)
+  .then(()=>res.redirect('/admin/products'))
+  .catch(err => console.log(err));
 };
