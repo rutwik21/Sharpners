c958a09c97d4f592cc1ea2a58b1ab188b7c7a275
rutwik21 <rutwikkashid000@gmail.com>
2023-07-03 13:45:30 +0530

    sequalize.transaction implimented

--- Expence Tracker/controllers/expence.js
+++ Expence Tracker/controllers/expence.js
@@ -1,21 +1,27 @@
 const expenceTable = require('../models/expence');
 const usertable = require('../models/user');
+const sq = require('../util/connection');
 const jwt = require('jsonwebtoken');
 
 exports.addExpence = async(req,res,next)=>{
+    const t = await sq.transaction(); 
     const amount = req.body.amount;
     const description = req.body.description;
     const category = req.body.category;
     const userId = jwt.verify(req.body.token,'secretkey');
 
     try{
-        await expenceTable.create({amount : amount,description : description,category : category, userId : userId});
+        await expenceTable.create({amount : amount,description : description,category : category, userId : userId},{transaction: t});
         const user = await usertable.findByPk(userId);
         const updatetotal = user.totalExpence +Number(amount);
         user.totalExpence = updatetotal;
-        user.save();
-        res.end()
-    }catch(err){console.log(err);res.status(401)};
+        await user.save({transaction:t});
+        await t.commit();
+        res.end();
+    }catch(err){
+        await t.rollback();
+        console.log(err);
+        res.status(401)};
 };
 
 exports.getExpence = async(req,res,next)=>{
@@ -26,8 +32,21 @@ exports.getExpence = async(req,res,next)=>{
 exports.deleteExpence = async(req,res,next)=>{
     try{
         const id = req.params.id;
-        let ex = await expenceTable.findByPk(id);
-        ex.destroy();
+        const ex = await expenceTable.findByPk(id);
+        const t = await sq.transaction(); 
+
+        const userId = ex.userId;
+        const user = await usertable.findByPk(userId);
+        const updatetotal = user.totalExpence - Number(ex.amount);
+        user.totalExpence = updatetotal;
+        await user.save({transaction:t});
+
+        await ex.destroy();
+        await t.commit();
         res.end();
-    }catch(err){console.log(err)};
+    }catch(err){
+        await t.rollback();
+        console.log(err);
+        res.json({error : err});
+    };
 };
