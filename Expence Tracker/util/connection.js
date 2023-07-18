28ee54d9c0128ae4ff938f6af36146e14e04997f
rutwik21 <rutwikkashid000@gmail.com>
2023-07-18 15:20:08 +0530

    Deployment ready

--- Expence Tracker/util/connection.js
+++ Expence Tracker/util/connection.js
@@ -1,8 +1,9 @@
 const sequelize = require('sequelize');
+require('dotenv').config();
 
-const sq = new sequelize('expence', 'root', 'rutwik', {
+const sq = new sequelize(process.env.DB_NAME,process.env.DB_USERNAME , process.env.DB_PASSWORD, {
     dialect: 'mysql',
-    host: 'localhost'
+    host: process.env.DB_HOST
   });
 
 module.exports = sq;
