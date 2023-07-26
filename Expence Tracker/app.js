262efa37d288b454965e25834600586d39c965b0
rutwik21 <rutwikkashid000@gmail.com>
2023-07-26 21:41:31 +0530

    changes made

--- Expence Tracker/app.js
+++ Expence Tracker/app.js
@@ -34,21 +34,6 @@ app.use(helmet({
 }));
 app.use(morgan('combined', {stream: accessLogStream}))
 app.use(express.json());
-
-function isSecure(req) {
-  if (req.headers['x-forwarded-proto']) {
-    return req.headers['x-forwarded-proto'] === 'https';
-  }
-  return req.secure;
-};
-
-app.use((req, res, next) => {
-  if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'test' && !isSecure(req)) {
-    res.redirect(301, `https://${req.headers.host}${req.url}`);
-  } else {
-    next();
-  }
-});
 app.use(express.static('views'));
 
 app.use('/user',userRoute);
